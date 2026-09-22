c0ea45cfcd463b27536d7f1213772e91ac5f559d
forfrags <114749872+forfrags@users.noreply.github.com>
2026-09-22 20:42:48 +0200

    Update practica_clase_magatzem.py

--- miprimerproyecto/practica_clase_magatzem.py
+++ miprimerproyecto/practica_clase_magatzem.py
@@ -1,14 +1,14 @@
-cabecera = ("{:^70}\n{:30}{:>10}{:>10}{:>10}\n{}\n".format(
-    "Magatzem Logistic",
+cabecera = ("{}\n{:30}{:>10}{:>10}{:>10}\n{}\n".format(
+    "Magatzem Logistic".center(60),
     "Producte", "Quantitat", "Preu", "Total",
-    "*" * 70))
+    "*" * 60))
 articulo = input("Dame un artículo: ")
 cantidad = int(input("Dame la cantidad: "))
 precio = float(input("Dame el precio: "))
 total = cantidad * precio
 datos = "{:30}{:>10}{:>10.2f}{:>10.2f}\n".format(
     articulo, cantidad, precio, total)
-print("*" * 70)
+print("*" * 60)
 print(cabecera + datos)
 print("{:>60}\n{:>60.2f}".format("Total Inventari", total))
 articulo = input("Dame un artículo: ")
@@ -17,7 +17,7 @@ precio = float(input("Dame el precio: "))
 total = cantidad * precio
 datos = "{:30}{:>10}{:>10.2f}{:>10.2f}\n".format(
     articulo, cantidad, precio, total)
-print("*" * 70)
+print("*" * 60)
 print(cabecera + datos)
 print("{:>60}\n{:>60.2f}".format("Total Inventari", total))
 articulo = input("Dame un artículo: ")
@@ -26,7 +26,6 @@ precio = float(input("Dame el precio: "))
 total = cantidad * precio
 datos = "{:30}{:>10}{:>10.2f}{:>10.2f}\n".format(
     articulo, cantidad, precio, total)
-print("*" * 70)
+print("*" * 60)
 print(cabecera + datos)
 print("{:>60}\n{:>60.2f}".format("Total Inventari", total))
-
